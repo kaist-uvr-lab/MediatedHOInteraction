171845e4c922764097207264737c16dd167e52b5
WJ.CHO <woojin.cho@kaist.ac.kr>
2025-09-04 11:09:43 +0900

    .

--- Server/main_create_seq_data.py
+++ Server/main_create_seq_data.py
@@ -9,7 +9,7 @@ import struct
 import json
 from PIL import Image, ImageDraw, ImageFont
 
-from modules import HandTracker_our_v2, identify_interacting_finger # GestureClassfier
+from modules import HandTracker_our_v2 # GestureClassfier
 from utils.visualize import draw_2d_skeleton
 
 sys.path.append("./hl2ss_")
@@ -24,19 +24,34 @@ import queue
 import keyboard
 import datetime
 import random
+from modules import HandTracker_our_v2
+from utils.visualize import draw_2d_skeleton
 
 
 #### args ####
 """
-subject 별로 gesture를 랜덤하게 이어서 수집. finger 구분은 따로해서 녹화.
-gesture 별로 10회 수집?
+subject 별로 gesture를 랜덤하게 이어서 수집. 
+
+물체 최소 2종, Finger 2개 별도로 수집. (최소 반복횟수 4회)
+
+일시정지 없음. 실수한 gesture idx기억해서 저장 후 삭제. 많이 누적되면 1회 더 수행하는 것으로 충당.
+
+한번 시행 당 2분 가량 소모.
+
+(주의)
+초록때 준비 끝내고, 빨간 원 나온후에 동작시작하도록 
+가급적 동작 준비할때는 물체에서 손가락을 뗴고 이동.
+
+
 
 """
 
 # Recording args
 subject = 0
-trial = 0
-finger_idx = 0
+
+trial = 1       # 2 object or more
+finger_idx = 1  # 2 type
+
 
 # fixed part
 fingers = ['thumb', 'index']
@@ -67,8 +82,8 @@ prev_label, prev = "Init", time.time()
 
 
 record_duration_per_gesture = 1.5
-pause_duration_per_gesture = 3.0
-num_per_action = 1
+pause_duration_per_gesture = 2.5
+num_per_action = 3
 actions_to_collect = []
 
 for i in range(num_per_action):
@@ -115,6 +130,8 @@ def main():
     depth_frames = []
     label_log = {}
 
+    track_hand = HandTracker_our_v2()
+
     try:
         while True:
             start_t = time.time()
@@ -137,14 +154,15 @@ def main():
 
             ### save buffer ###
             if flag_init:
-                color_frames.append((frame_idx, color.copy()))
+                color_frames.append((current_action_index, frame_idx, color.copy()))
                 if flag_depth:
-                    depth_frames.append((frame_idx, depth.copy()))
+                    depth_frames.append((current_action_index, frame_idx, depth.copy()))
                 frame_idx += 1
 
             ### Display RGB ###
             cv2.imshow('RGB', color)
             cv2.waitKey(1)
+
 
             ###################### receive keyboard input ######################
             if keyboard.is_pressed('space') and flag_once:
@@ -160,7 +178,22 @@ def main():
                 print("exiting ...")
                 break
 
+
+            ###################### run tracker ######################
+
+            outs = track_hand.run(color)
+            if not outs:
+                print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] No hand visible")
+                continue
+            else:
+                all_right, all_uvds, _, _ = outs
+                indices = np.where(np.asarray(all_right) == 1)[0]  ### check. 0: left, 1: right
+                if len(indices) != 0:
+                    uvd_right = np.squeeze(np.asarray(all_uvds)[indices[0]])
+                    color = draw_2d_skeleton(color, uvd_right)
+
             ###################### show prompt ######################
+
 
             if flag_init:
                 if flag_recording:
@@ -174,7 +207,7 @@ def main():
                     cv2.circle(color, (pv_width - 30, 30), 15, (0, 0, 255), -1)
 
                     # 왼쪽 위 텍스트
-                    cv2.putText(color, f"{target_finger} - {action_text} ({current_action_index+1}/{total_len})", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 0) , 2)
+                    cv2.putText(color, f"{target_finger} - {action_text} ({current_action_index+1}/{total_len})", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0) , 2)
 
                     # 오른쪽 빨간 바 (시간에 따라 줄어듦)
                     bar_height = int((1 - elapsed / record_duration_per_gesture) * pv_height)
@@ -198,7 +231,7 @@ def main():
                         wait_text = f"Ready for ...{target_finger} - {next_action}"
                     else:
                         wait_text = "all done"
-                    cv2.putText(color, wait_text, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 0), 2)
+                    cv2.putText(color, wait_text, (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
 
                     # 오른쪽 초록 바 (시간에 따라 줄어듦)
                     bar_height = int((1 - elapsed / pause_duration_per_gesture) * pv_height)
@@ -231,8 +264,9 @@ def main():
             latency = time.time() - start_t
             print(f"added fps : { 1/latency}, ms : {latency*1000}")
 
-        print("saving images in buffer ...")
         ### save images ###
+        print("saving images in buffer ...")
+
         color_image_dir = f"dataset/subject_{subject}/trial_{trial}/{target_finger}/rgb"  # 저장할 디렉토리
 
         if os.path.exists(color_image_dir):
@@ -241,14 +275,20 @@ def main():
             color_image_dir = f"dataset/subject_{subject}/trial_{trial}/{target_finger}/rgb"  # 저장할 디렉토리
 
         os.makedirs(color_image_dir, exist_ok=True)
-        for frame_idx, color in color_frames:
-            out_path = os.path.join(color_image_dir, f"frame_{frame_idx:06d}.png")
+        for action_index, frame_idx, color in color_frames:
+            more_path = os.path.join(color_image_dir, f"{action_index+1}_{actions_to_collect[action_index]}")
+            os.makedirs(more_path, exist_ok=True)
+
+            out_path = os.path.join(more_path, f"frame_{frame_idx:06d}.png")
             cv2.imwrite(out_path, color)
 
         depth_image_dir = f"dataset/subject_{subject}/trial_{trial}/{target_finger}/depth"  # 저장할 디렉토리
         os.makedirs(depth_image_dir, exist_ok=True)
-        for frame_idx, depth in depth_frames:
-            out_path = os.path.join(depth_image_dir, f"depth_{frame_idx:06d}.npy")
+        for action_index, frame_idx, depth in depth_frames:
+            more_path = os.path.join(depth_image_dir, f"{action_index+1}_{actions_to_collect[action_index]}")
+            os.makedirs(more_path, exist_ok=True)
+
+            out_path = os.path.join(more_path, f"depth_{frame_idx:06d}.npy")
             np.save(out_path, depth.astype(np.float32))  # float32 그대로 저장
 
         with open(f"dataset/subject_{subject}/trial_{trial}/{target_finger}/label.txt", 'w', encoding='utf-8') as f:
